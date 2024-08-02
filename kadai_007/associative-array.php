f7db34caf88b093c90e0abfda44551430a1ec783
Sirasu-02 <167178469+Sirasu-02@users.noreply.github.com>
2024-08-02 15:20:55 +0900

    kadai_007アップ

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+  </head>
+
+  <body>
+
+    <p>
+
+      <?php
+        //連想配列
+        $product_Information = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        //出力
+        print_r($product_Information);
+
+      ?>
+
+    </p>
+
+  </body>
+
+</html>
